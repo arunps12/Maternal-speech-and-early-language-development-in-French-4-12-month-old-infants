d37d09df25e0df4723351e03e1f845e52ffaeb1c
arunps12 <mail.arun1289@gmail.com>
2024-10-23 15:55:29 +0200

    Add path to save acoustic measures

--- path.py
+++ path.py
@@ -1,4 +1,10 @@
 import os
+from utils import create_dir
 
 # Base directory of the project
 basedir = os.path.abspath(os.path.dirname(__file__)) 
+
+feat_file = os.path.join(basedir, 'features_SES_age_sex.xlsx')
+
+acoustic_measures_dir = os.path.join(basedir, 'acoustic_measures')
+create_dir(acoustic_measures_dir)
