8097f2d8a83c4d643df8916e4ce9e0112489e9eb
arunps12 <mail.arun1289@gmail.com>
2024-10-23 15:57:01 +0200

    Add a function to convert Hz to semitones

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 import os
+import numpy as np
 
 def create_dir(dir_name):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
+
+def Hz_to_semitones(Hz_values, ref):
+    return 12 * np.log2(Hz_values/ref)
