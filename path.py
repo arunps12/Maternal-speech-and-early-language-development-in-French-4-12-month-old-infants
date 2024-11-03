41859a51fa02a4f0e309a072261c925adf09f6e1
arunps12 <mail.arun1289@gmail.com>
2024-11-03 14:22:07 +0100

    file to create paths

--- path.py
+++ path.py
@@ -8,3 +8,7 @@ feat_file = os.path.join(basedir, 'features_SES_age_sex.xlsx')
 
 acoustic_measures_dir = os.path.join(basedir, 'acoustic_measures')
 create_dir(acoustic_measures_dir)
+plot_vow_measures_dir = os.path.join(basedir, 'plot_acoustic_measures')
+create_dir(plot_vow_measures_dir)
+plot_stat_analyses_dir = os.path.join(basedir, 'StatPlots')
+create_dir(plot_stat_analyses_dir)
